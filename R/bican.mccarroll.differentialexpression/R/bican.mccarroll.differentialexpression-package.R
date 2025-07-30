88a7f7b3fc52fd0035090b51ae4bc60ab6b9a319
jamesnemesh <nemesh@broadinstitute.org>
2025-07-30 11:30:23 -0400

    Fixed dependencies so bioconductor packages are installed via remotes.
    Updated description and packaage index for documentation

---
+++ R/bican.mccarroll.differentialexpression/R/bican.mccarroll.differentialexpression-package.R
@@ -0,0 +1,8 @@
+#' bican.mccarroll.differentialexpression
+#'
+#' Tools for differential expression and visualization of single-cell RNA-seq data
+#' for BICAN. Provides functions for running differential expression using edgeR
+#' and variancePartition, and visualization using GlimmaV2.
+#'
+#' @keywords internal
+"_PACKAGE"
