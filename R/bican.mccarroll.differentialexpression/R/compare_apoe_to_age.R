ec2140087fc6e3c18008c29f24332d8cfea4eda9
jamesnemesh <nemesh@broadinstitute.org>
2025-12-10 09:18:24 -0500

    Comparisons of age to apoe score, added some QQPlots.

--- R/bican.mccarroll.differentialexpression/R/compare_apoe_to_age.R
+++ R/bican.mccarroll.differentialexpression/R/compare_apoe_to_age.R
@@ -1,8 +1,8 @@
 # A little ad-hoc analysis of APOE status DE vs age DE
 
-age_dir="/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/differential_expression/results/LEVEL_2_apoe_score/sex_age/cell_type"
-apoe_dir="/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/differential_expression/results/LEVEL_2/sex_age/cell_type"
-cell_type="astrocyte"
+# age_dir="/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/differential_expression/results/old//LEVEL_2_apoe_score/sex_age/cell_type"
+# apoe_dir="/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/differential_expression/results/old/LEVEL_2/sex_age/cell_type"
+# cell_type="microglia"
 
 compare_apoe_to_age_de<-function (age_dir, apoe_dir, cell_type) {
 
@@ -18,10 +18,14 @@ compare_apoe_to_age_de<-function (age_dir, apoe_dir, cell_type) {
     age=read.table(age_file, header=TRUE, sep="\t", stringsAsFactors = FALSE)
     apoe=read.table(apoe_file, header=TRUE, sep="\t", stringsAsFactors = FALSE)
 
-    #age=age[age$adj.P.Val<0.05, ]
     both=intersect (rownames (age), rownames (apoe))
     age=age[both, ]
     apoe=apoe[both, ]
+
+    #subset to results significant in either
+    sig_age=which(age$adj.P.Val<0.05 | apoe$adj.P.Val <0.05)
+    age=age[sig_age, ]
+    apoe=apoe[sig_age, ]
 
     plot (age$logFC, apoe$logFC, xlab="Age logFC", ylab="APOE logFC", main=paste0(" Age vs APOE DE in ", cell_type))
 
