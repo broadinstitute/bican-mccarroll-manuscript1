e3ea6636799096adc35c43ed68fa731b4205ebf9
jamesnemesh <nemesh@broadinstitute.org>
2026-02-25 09:09:41 -0500

    Updates to inst/script plotting code to point to the package
    Updates figures trade plots to finalize.
    
    Finished differential expression donor age plots (last few DE plots for manuscript)

--- R/bican.mccarroll.figures/R/donor_expression_age_plots.R
+++ R/bican.mccarroll.figures/R/donor_expression_age_plots.R
@@ -1,150 +1,16 @@
-source("R/paths.R")
+# source("R/paths.R")
+#
+# options(
+#     bican.mccarroll.figures.data_root_dir =
+#         "/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis",
+#
+#     bican.mccarroll.figures.out_dir =
+#         "/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/figure_repository",
+#
+#     bican.mccarroll.figures.cache_dir =
+#         "/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/figure_repository/data_cache"
+# )
 
-options(
-    bican.mccarroll.figures.data_root_dir =
-        "/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis",
-
-    bican.mccarroll.figures.out_dir =
-        "/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/figure_repository",
-
-    bican.mccarroll.figures.cache_dir =
-        "/broad/bican_um1_mccarroll/RNAseq/analysis/CAP_freeze_3_analysis/figure_repository/data_cache"
-)
-
-
-
-#' Generate donor-level GEX plots vs age
-#'
-#' This function computes all required intermediate objects (with RDS caching)
-#' and then generates the manuscript plots (heatmap, scatters, and the 5x6 panel grid).
-#' It does not save files; it prints plots to the active device.
-#'
-#' @param ct_file Path to cell types list. If NULL, resolved by resolver.
-#' @param cell_metadata_file Path to annotated cell metadata. If NULL, resolved by resolver.
-#' @param metacells_file Path to metacells DGEList counts. If NULL, resolved by resolver.
-#' @param outDir Output directory. If NULL, resolved by resolver.
-#' @param data_cache_dir Cache directory. If NULL, resolved by resolver.
-#'
-#' @return Invisibly NULL.
-# plot_donor_gex_vs_age <- function(ct_file = NULL,
-#                                   cell_metadata_file = NULL,
-#                                   metacells_file = NULL,
-#                                   outDir = NULL,
-#                                   data_cache_dir = NULL) {
-#
-#     paths <- .resolve_donor_gex_age_paths(
-#         ct_file = ct_file,
-#         cell_metadata_file = cell_metadata_file,
-#         metacells_file = metacells_file,
-#         outDir = outDir,
-#         data_cache_dir = data_cache_dir
-#     )
-#
-#     cache <- load_or_build_donor_gex_age_cache(paths)
-#     donor_ages <- cache$donor_ages
-#     metacell_cr_list <- cache$metacell_cr_list
-#
-#     ## -----------------------
-#     ## Plots
-#     ## -----------------------
-#
-#     gene_list <- c("GPNMB","CD163","CD163L1","MS4A4A","MS4A6A","CYP27A1","CLEC5A","CLEC2B","FPR3","GPR141","DOCK5","IQGAP2","IL15","CYTL1","MAFB","FOXP1","ESR1","THRB")
-#     heatmap_key <- "microglia__CaH"
-#     gs_gaps <- c(6,12,14)
-#
-#     #This uses pheatmap, so needs to captured by the current plotting device.
-#     # save to SVG donor_age_microglia_heatmap.svg
-#     p_heatmap <- bican.mccarroll.de.analysis::plot_donor_gex_age_heatmap(
-#         metacell_cr_list[[heatmap_key]],
-#         gs = gene_list,
-#         donor_ages = donor_ages,
-#         gs_gaps = gs_gaps,
-#         cluster_gs = FALSE,
-#         transpose = FALSE
-#     )
-#
-#     gene_list <- c("PCLO","CAPS2","STX3","MCTP2","JPH1","SLIT1","SEMA5A","TSHZ3","IQGAP1","SIPA1L2","CTNNAL1","DNMBP","KIF13B","EPB41L4A","LAMB1","FBLN5","TLL1","ABI3BP","PTPN13","PTPRK","DENND3","LRRK1","MKNK1","SGSM1","ABCG1","LEPR","C2","PPP1R1B","GPR88","RGS9","RGS12","ADCY3","PDE1B","PKIA","KCNQ3","SCN4B","SYT6","SYT12","LRRTM3","SYNDIG1","ASTN2","PCDH9","EPHA5","SLITRK2","ADGRB2","CSMD2","HMGCS1","MSMO1","MICOS10","PEX5","ABCD2","SAT1","DCX","FOXG1")
-#     heatmap_key <- "MSN_D1_matrix__CaH"
-#     gs_gaps <- c(5,8,14,18,24,27,34,36,46,52)
-#
-#     # save to SVG donor_age_D1_matrix_heatmap.svg
-#     bican.mccarroll.de.analysis::plot_donor_gex_age_heatmap(
-#         metacell_cr_list[[heatmap_key]],
-#         gs = gene_list,
-#         donor_ages = donor_ages,
-#         gs_gaps = gs_gaps,
-#         cluster_gs = FALSE,
-#         transpose = FALSE
-#     )
-#
-#     #############################################
-#     # Big scatter plot/table of genes/cell types
-#     #############################################
-#
-#     gene_list <- c("FKBP5", "RGS9", "RYR3", "GRIA1", "CLEC2B")
-#
-#     keys <- c("MSN_D1_matrix__CaH", "glutamatergic_L23IT__DFC", "astrocyte__CaH",
-#         "OPC__CaH", "oligodendrocyte__CaH", "microglia__CaH")
-#
-#     nice_names <- c("D1 matrix MSN", "L23IT glutamatergic\nneuron", "Astrocyte",
-#         "OPC", "Oligodendrocyte", "Microglia")
-#
-#     plot_list <- list()
-#     idx <- 0L
-#     n_row <- length(gene_list)
-#     n_col <- length(keys)
-#
-#     for (r in seq_len(n_row)) {
-#         for (c in seq_len(n_col)) {
-#             idx <- idx + 1L
-#             plot_list[[idx]] <- make_panel(
-#                 metacell_cr_list = metacell_cr_list,
-#                 key = keys[c],
-#                 gene = gene_list[r],
-#                 donor_ages = donor_ages,
-#                 show_x = (r == n_row),
-#                 show_spearman = TRUE,
-#                 spearman_text_size = 4
-#             )
-#         }
-#     }
-#
-#     grid <- cowplot::plot_grid(
-#         plotlist = plot_list,
-#         nrow = n_row,
-#         align = "hv"
-#     )
-#
-#     col_label_grobs <- lapply(
-#         nice_names,
-#         function(x) cowplot::ggdraw() + cowplot::draw_label(x, size = 12)
-#     )
-#     col_labels <- cowplot::plot_grid(plotlist = col_label_grobs, nrow = 1)
-#
-#     grid_with_cols <- cowplot::plot_grid(
-#         grid,
-#         col_labels,
-#         ncol = 1,
-#         rel_heights = c(1, 0.08)
-#     )
-#
-#     row_label_grobs <- lapply(
-#         gene_list,
-#         function(g) cowplot::ggdraw() + cowplot::draw_label(g, angle = 90, size = 14)
-#     )
-#     row_labels <- cowplot::plot_grid(plotlist = row_label_grobs, ncol = 1)
-#
-#     #save to SVG donor_age_scatterplot_grid.svg
-#     all_plot <- cowplot::plot_grid(
-#         row_labels,
-#         grid_with_cols,
-#         nrow = 1,
-#         rel_widths = c(0.06, 1)
-#     )
-#
-#
-#     invisible(NULL)
-# }
 
 #' Generate donor-level GEX plots vs age
 #'
